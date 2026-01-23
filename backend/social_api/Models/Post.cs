7c4edfff1d1a178973dc880f49d4d12207456100
QutaibaAbualrob <qotaibawaleed@gmail.com>
2026-01-23 14:15:40 +0200

    Include post

--- backend/social_api/Models/Post.cs
+++ backend/social_api/Models/Post.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 using SocialMediaAPI.Enums;
+using SocialMediaAPI.Models;
 
 namespace SocialMediaAPI
 {
@@ -10,13 +11,16 @@ namespace SocialMediaAPI
     {
         [Key]
         public int PostId {get; set;}
-        [ForeignKey]
+
+        
         public int UserId {get; set;}
 
         [MaxLength(5000)]
-        public string Content {get; set;}
+        public string? Content {get; set;}
 
         public  MediaType MediaType {get; set;}
+        
+        public string? MediaURL {get; set;}
         
         public DateTime TimeStamp {get; set;}
 
