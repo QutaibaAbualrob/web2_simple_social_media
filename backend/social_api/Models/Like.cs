497b596677bd1000819572c980a88b60b0c50502
QutaibaAbualrob <qotaibawaleed@gmail.com>
2026-01-23 17:12:48 +0200

    Remove ternary from postid, userid in likes mode

--- backend/social_api/Models/Like.cs
+++ backend/social_api/Models/Like.cs
@@ -11,10 +11,10 @@ namespace SocialMediaAPI.Models
     {
         [Key]
         public int LikeId {get; set;}
-        public int? PostId {get; set;}
+        public int PostId {get; set;}
 
         public int? CommentId {get; set;}
-        public int? UserId {get; set;}
+        public int UserId {get; set;}
 
         
         public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
